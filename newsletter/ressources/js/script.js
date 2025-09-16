338ac3caf9d32fa5c7cae5b21090aad45d00aebe
Rosa <costurezati@gmail.com>
2025-09-16 15:11:15 -0300

    .

--- newsletter/ressources/js/script.js
+++ newsletter/ressources/js/script.js
@@ -4,61 +4,49 @@ const msgSucesso = document.querySelector(".msg_sucesso");
 const modalEnviar = document.querySelector(".modal_enviar");
 const btnEnviar = document.querySelector(".btn_enviar");
 
+const validadarDados = ({ nome, email }) => {
+    //validar dados
+    const nomeValido = nome && nome.length >= 3;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const emailValido = email && emailRegex.test(email);
+
+    return{
+        nomeValido,
+        emailValido
+    }
+
+};
 const pegarDados = () => {
     //dados formulario
-    const nome = document.querySelector(".input_nome").value;
-    const email = document.querySelector(".input_email").value;
-    console.log(nome, email);
-    let nomeValido = false;
-    let emailValido = false;
+    const dados = {
+        nome: document.querySelector(".input_nome").value,
+        email: document.querySelector(".input_email").value,
+    };
 
-    //validar dados
-    if(nome === "" || nome === nul || nome.length <3 ){
-        document.querySelector('.erro_nome').textContent = "O nome é obrigatório e precisa ter no mínimo 3 caracteres";
-    }else{
-        nomeValido = true;
-        document.querySelector('.erro_nome').textContent = "";
-    }
+    const {nomeValido, emailValido} = validadarDados(dados);
 
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const resultado = nomeValido && emailValido ? 'sucesso' : 'erro';
+    document.querySelector('form').reset();
 
-    if(!emailRegex.test(email)){
-        document.querySelector('.erro_email').textContent = "O e-mail é obrigatório e precisar ser um e-mail válido.";
-    }else{
-        emailValido = true;
-        document.querySelector('.erro_email').textContent = "";
-    }
-
-    if(nomeValido && emailValido){
-        const cadastro = {
-            nome,
-            email
-        }
-        console.log(cadastro);
-        document.querySelector('form').reset();
-        return 'sucesso';
-    }else{
-        return 'erro';
-    }
+    return resultado;
 };
 
-const mostrarModal = (statusRegister) => {
-    //formatação da janela modal
-
-
-    if (statusRegister === 'sucesso') {
-        msgErro.style.display = 'none';
-        msgSucesso.style.display = 'block';
+const formatarModal = (statusRegister) => {
+    msgSucesso.style.display = (statusRegister === "sucesso") ? 'block' : 'none';
+    msgErro.style.display = (statusRegister === "erro") ? 'block' : 'none';
+    
+    // Correção da manipulação de classes
+    if (statusRegister === "sucesso") {
         btnFechar.classList.add('bg_sucesso');
         btnFechar.classList.remove('bg_erro');
-    }
-    if (statusRegister === 'erro') {
-        msgSucesso.style.display = 'none';
-        msgErro.style.display = 'block';
+    } else {
         btnFechar.classList.add('bg_erro');
         btnFechar.classList.remove('bg_sucesso');
     }
-
+};
+const mostrarModal = (statusRegister) => {
+    //formatação da janela modal
+    formatarModal(statusRegister);
     modalEnviar.showModal();
 };
 
